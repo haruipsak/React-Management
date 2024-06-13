5dede98d1270bdf064c939f42be47dda2c0003f3
haruipsak <zzzdahee@gmail.com>
2024-06-13 15:11:56 +0900

    커밋합니다요

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Customer from "./components/Customer";
+
+const customers = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/1",
+    name: "유아로",
+    birthday: "220428",
+    gender: "수컷",
+    job: "고양이",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2",
+    name: "모몰로",
+    birthday: "230128",
+    gender: "수컷",
+    job: "고양이",
+  },
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/3",
+    name: "모쨰순",
+    birthday: "950211",
+    gender: "남자",
+    job: "사람",
+  },
+];
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>Let's develop mangaement system!!!!</h2>
-    
+    <div>
+      {customers.map((c) => {
+        return (
+          <Customer
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+          ></Customer>
+        );
+      })}
     </div>
   );
 }
