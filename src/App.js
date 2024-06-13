38b3cd0f3046d2e8730a07bd6de3c49308550286
haruipsak <zzzdahee@gmail.com>
2024-06-13 16:04:53 +0900

    함수형 컴포넌트, hook 적용

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Customer from "./components/Customer";
+import {
+  Paper,
+  Tab,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  makeStyles,
+} from "@mui/material";
+import { Component } from "react";
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    marginTop: 3, // theme.spacing.unit 대신 간단한 숫자 사용 가능
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 1080,
+  },
+});
 
 const customers = [
   {
@@ -29,24 +51,40 @@ const customers = [
   },
 ];
 
-function App() {
+const App = () => {
+    const classes = useStyles();
   return (
-    <div>
-      {customers.map((c) => {
-        return (
-          <Customer
-            key={c.id}
-            id={c.id}
-            image={c.image}
-            name={c.name}
-            birthday={c.birthday}
-            gender={c.gender}
-            job={c.job}
-          ></Customer>
-        );
-      })}
-    </div>
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {customers.map((c) => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              ></Customer>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </Paper>
   );
 }
+
 
 export default App;
